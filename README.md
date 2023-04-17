cddc6f771342bd18563f23683490082cf58c06ef
Dusan95z <dusan204z@gmail.com>
2023-04-17 18:42:41 +0200

    app finished

---
+++ README.md
@@ -0,0 +1,4 @@
+6 - Forkify App
+
+Project done with HTML, SCSS and Javascript.
+Focus on OOP and AJAX.
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2ac37d21-6f4d-4323-ae4c-1e82b39d483a';
+export const MODAL_CLOSE_SEC = 2.5;
